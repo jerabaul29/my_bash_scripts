70173be6da1e7aaa93f737c9bd0570b8e486bd64
jerabaul29 <jean.rblt@gmail.com>
2018-04-13 10:18:56 +0200

    working histg and histx, but code need improvements

--- histg.sh
+++ histg.sh
@@ -12,9 +12,20 @@ if [ "$1" == "-h" ]; then
   exit 0
 fi
 
+# TODO: automate how to recover HISTSIZE and share it with bash_rc
 HISTSIZE=100000            # need this, because does not read .bashrc so does not know how big a HISTSIZE
 HISTFILE=~/.bash_history   # Or wherever you bash history file lives
 set -o history             # enable history
 
-OUTPUT="$(history | grep --color=always $1 | sort -k2 | uniq -f 1 | sort -n | awk '{printf("% 4d  %s\n", NR, $0)}' )"
+# TODO: automate how to set the PATH_TO_SAVELAST and share it with histx
+PATH_TO_SAVELAST="/home/jrlab/Desktop/Git/MyBashScripts/Data/last_histg_output"
+
+# TODO: use the history number corresponding to the LAST time the command was used
+echo "issuing: ${COMMAND}"
+OUTPUT="$(history | grep --color=always "$1" | sort -k2 | uniq -f 1 | sort -n | awk '{printf("% 4d  %s\n", NR, $0)}')"
 echo "${OUTPUT}"
+
+# TODO: rather than running again, use ${OUTPUT}
+OUTPUT_NOCOLOR="$(history | grep "$1" | sort -k2 | uniq -f 1 | sort -n | awk '{printf("% 4d  %s\n", NR, $0)}')"
+> ${PATH_TO_SAVELAST}
+echo "${OUTPUT_NOCOLOR}" | cat >> ${PATH_TO_SAVELAST}
--- histx.sh
+++ histx.sh
@@ -3,11 +3,6 @@
 # TODO: no need to type the last histg pattern
 # use: grep histg, and tail, and grep a given field with awk '{print $2}'
 
-if [ $# -eq 0 ]; then
-    echo "No arguments provided; -h for help"
-    exit 1
-fi
-
 if [ "$1" == "-h" ]; then
   echo "A bash script to execute the n-th command shown by histg"
   echo "use: histx number"
@@ -15,7 +10,9 @@ if [ "$1" == "-h" ]; then
   exit 0
 fi
 
-OUTPUT=$(~/Desktop/Git/MyBashScripts/histg.sh $1)
-STRINGTEST=`echo "${OUTPUT}" | head -$2 | tail -1 | cut -c 14-`
-echo "issuing ${STRINGTEST}"
-eval ${STRINGTEST}
+# TODO: automate how to set the PATH_TO_SAVELAST and share it with histx
+PATH_TO_SAVELAST="/home/jrlab/Desktop/Git/MyBashScripts/Data/last_histg_output"
+
+SELECTED_COMMAND="$(head -$1 ${PATH_TO_SAVELAST} | tail -1 | cut -c 14-)"
+echo "SELECTED_COMMAND ${SELECTED_COMMAND}"
+eval ${SELECTED_COMMAND}
