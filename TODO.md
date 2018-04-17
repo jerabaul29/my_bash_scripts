f7a669fe0bb32fc1b2c3fb228af3ff6a416ff2f2
jerabaul29 <jean.rblt@gmail.com>
2018-04-17 17:31:48 +0200

    all working

---
+++ TODO.md
@@ -0,0 +1,4 @@
+# TODO
+
+- re write histg and histx as functions + update readme?
+- note: function vs script (subshell vs exec in same shell)
--- zx.sh
+++ zx.sh
@@ -2,11 +2,16 @@
 
 # in order to be able to do the change of the parent terminal, must be a function not a script
 function zx() {
+    if [ $# -eq 0 ]; then
+        echo "No arguments provided; -h for help"
+        return
+    fi 
+
     if [ "$1" == "-h" ]; then
-    echo "A bash script to change path according to the result of zg"
-    echo "use: zx number"
-    echo "ex : zx 4"
-    exit 0
+        echo "Change path according to the result of zg"
+        echo "use: zx number"
+        echo "ex : zx 4"
+        return
     fi
 
     # TODO: automate how to set the PATH_TO_SAVELAST and share it with zx
